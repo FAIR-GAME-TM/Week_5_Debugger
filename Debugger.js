6d15a37de1059b04d95b7f0fc8737773dca57458
Fair-Game-TM <157636823+FAIR-GAME-TM@users.noreply.github.com>
2024-11-06 14:21:49 -0700

    Add files via upload

---
+++ Debugger.js
@@ -0,0 +1,55 @@
+
+// function myFunction(){
+//     document.getElementById("Javascript").innerHTML = "You Loose";
+//     alert("You did not win, and this is my first time coding, thank you prof battana and levi");
+// }
+
+
+/*
+This is a multi-line comment
+It can span multiple lines
+
+function validateform(){
+const username = document.getElementById('username').value;
+const password = document.getElementById('password').value;
+const confirmPssword = document.getElementById('confirmpassword').value;
+
+if(password !== confirmPssword){
+    alert("Passwords do not match");
+    return;
+}
+else {
+    alert("Registration was Succesfull");
+}
+
+document.getElementById('registrationForm').submit();
+}
+-->*/
+
+function myFunction() { 
+    document.getElementById("randomword").innerHTML = "But My name is Larry"; 
+} 
+alert
+var dice1 = 5;
+var dice2 = 5; 
+var sum = dice1+dice2;
+console.log('Sum:' + ' ' + sum);  
+if(sum == 7 || sum == 11){  
+    console.log("Win");  
+}  
+else if (dice1 == dice2 && dice1%dice2 == 0 ){  
+    console.log("Lose");  
+}  
+else{  
+    console.log("Try again");  
+} 
+
+let a = 6;
+let b = 9;
+let c = a * b;
+
+// stops the execution
+debugger;
+
+console.log(c);
+
